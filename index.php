6dfbe937e710ad1a6ffcdc259b848eddb54a4623
Calado <jricardoc10@gmail.com>
2015-04-08 14:21:22 -0300

    iniciando

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'oviniDeves.php';
+
+$cometas= array('HALLEY','ENCKE','WOLF','KUSHIDA');
+$grupos=array('AMARELO','VERMELHO','PRETO','AZUL');
+
+
+$grupos= new oviniDeves($cometas,$grupos);
+
+
+echo "<br> <h3>Grupos Levados para uma gal&aacute;xia distante</h3>";
+
+foreach($grupos->getGruposLevados() as $grupo){
+	echo "<br>".$grupo;
+} 
+
+echo "<hr/>";
+echo "<br> <h3>Grupos n&atilde;o Levados</h3>";
+
+
+foreach($grupos->getGruposNaoLevados() as $grupo){
+	echo "<br>".$grupo;
+}
+
+
+
---
+++ oviniDeves.php
@@ -0,0 +1,67 @@
+<?php
+
+class oviniDeves
+{
+	
+protected $cometas =  array();
+protected $grupos  =  array();
+protected $alfabeto=  array();
+protected $mod=45;
+protected $gruposLevados=array();
+protected $gruposNaoLevados=array();
+
+	public function __construct($cometas,$grupos)
+    {
+        $this->cometas = $cometas;
+        $this->grupos = $grupos;
+		$this->alfabeto=$this->geraAlfabeto();
+		$this->separaGrupos();
+     }
+     
+    public function getGruposLevados(){
+    	return $this->gruposLevados;
+    } 
+    public function getGruposNaoLevados(){
+    	return $this->gruposNaoLevados;
+    } 
+
+   protected function separaGrupos(){
+
+     	foreach($this->cometas as  $key => $cometas ){
+        	$respCometa = $this->convert($cometas);
+     		$respGrupo  = $this->convert($this->grupos[$key]);
+     		
+     		if($respGrupo != $respCometa){
+     			$this->gruposNaoLevados[$key]=$this->grupos[$key];
+     		}else{
+     			$this->gruposLevados[$key]=$this->grupos[$key] ;
+     		}
+       	}
+     }
+	protected function convert($string)
+	{
+		$stringArray=str_split(strtoupper($string));
+		
+		foreach($stringArray as $key => $str){
+			$numero=array_search($str,$this->alfabeto);
+			if($key== 0){
+					$antrerior=$numero;
+			}else{
+					$antrerior=$numero*$antrerior;
+			}
+		}
+		$res = $antrerior % $this->mod;
+		return $res;
+	}
+	
+	
+	protected function geraAlfabeto(){
+		foreach(range('A', 'Z') as $letra) {
+			$i++;
+			$alfabeto[$i]=$letra;
+		}
+		return $alfabeto;	
+	}
+
+} 
+
